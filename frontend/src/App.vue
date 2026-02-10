004b05e7ade9e15d49604aae74edb29d825e2987
sofian.hussein@eduvaud.ch <sofian.hussein@eduvaud.ch>
2026-02-10 14:43:10 +0100

    chore(): initial project structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-green-400">
+      Casino
+    </h1>
+  </div>
+</template>
