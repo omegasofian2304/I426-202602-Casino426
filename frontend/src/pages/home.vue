7571735251eaa466461b97c494b6ac4d76db7238
Sofian Hussein <sofian.hussein@eduvaud.ch>
2026-02-11 11:09:34 +0100

    Feature/router (#57)
    * feat(): Add a router
    
    * feat(): Add a router

---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Accueil</h1>
+    <p>Bienvenue sur la page principale</p>
+  </div>
+</template>
+
+<script setup>
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+
+const routes = [
+    { path: '/Home', component: Home }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(),
+    routes
+});
 
-export default router
+export default router;
