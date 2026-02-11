88d73071fe2121620cb2f994814274fab39161c5
sofian.hussein@eduvaud.ch <sofian.hussein@eduvaud.ch>
2026-02-11 11:25:53 +0100

    feat(): Add a router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-    <h1 class="text-4xl font-bold text-green-400">
-      Casino
-    </h1>
-  </div>
+  <nav>
+    <router-link to="/Home">Home</router-link>
+  </nav>
+  <router-view />
 </template>
