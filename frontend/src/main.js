248669db0a3753b3fcff6dc5bd2d65263b1250f6
god <loic.roux@eduvaud.ch>
2026-02-25 09:30:01 +0100

    feat(): add the home page (#62)
    * feat(): add the home page
    
    * feat(): add the packages
    
    ---------
    
    Co-authored-by: py50eum <py50eum@eduvaud.ch>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
+import './assets/main.css'
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/index.js';
 import './assets/main.css'
 
 createApp(App)
