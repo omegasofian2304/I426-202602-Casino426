9fa474dde1874e757c21e233e06ed2955b7577ee
sofian.hussein@eduvaud.ch <sofian.hussein@eduvaud.ch>
2026-02-25 08:49:53 +0100

    feat(): Add the blackjack background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <nav>
     <router-link to="/Home">Home</router-link>
+    <router-link to="/Blackjack">Blackjack</router-link>
   </nav>
   <router-view />
 </template>
+<script setup lang="ts">
+</script>
