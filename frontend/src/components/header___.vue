555002e7d5991b7045f5afdd2126149380dfbdf4
Rodrigo <pi40qdk@eduvaud.ch>
2026-03-18 10:05:30 +0100

    feat(): add the back to menu button (#81)

--- frontend/src/components/header___.vue
+++ frontend/src/components/header___.vue
@@ -7,7 +7,7 @@ Project: Casino426
   */
 <template>
   <header class="w-full bg-[#cc1111] flex items-center h-13 px-4 relative overflow-hidden font-['Oswald'] text-white text-sm tracking-wide">
-    <router-link to="/home" class="text-[#000000]">
+    <router-link to="/home" class="text-[#000000]">   <!--Route pour retourner à l'accueil quand on clique sur le bouton -->
       <span class="z-10 whitespace-nowrap">Accueil</span>
     </router-link>
     <!-- Chevrons gauche -->
